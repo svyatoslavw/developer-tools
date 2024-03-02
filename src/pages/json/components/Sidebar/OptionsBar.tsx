c50601900d3151751138c33a3c49427279942d13
svyatoslavw <sadness0v2@gmail.com>
2024-03-03 00:40:18 +0200

    🐛 Added vercel.json

--- src/pages/json/components/Sidebar/OptionsBar.tsx
+++ src/pages/json/components/Sidebar/OptionsBar.tsx
@@ -2,7 +2,7 @@ import { OPTIONS_WITH_LINK } from "../../utils/constants/options.constant"
 
 const OptionsBar = () => {
   return (
-    <div className="flex w-96 flex-col border-r pr-4 text-sm text-primary/50">
+    <div className="flex w-96 flex-col border-r border-neutral-800 pr-4 text-sm text-primary/50">
       {OPTIONS_WITH_LINK.map((option) => (
         <a
           key={option.name}
